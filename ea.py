1376a290b04d5a8f4da5dd5770e238efbbebf1cb
100522322 <150807269+HPASTRAN4@users.noreply.github.com>
2026-03-23 18:54:59 +0100

    Commented EA

--- ea.py
+++ ea.py
@@ -23,36 +23,46 @@ class EA:
         Main loop which creates a log with the information.
         Pseudocode 2 implemented
         """
+        # Initialized the first Population
         factory = self.factory 
         self.population = Population(self.pop_size, factory.n_jobs, factory )
         self.best_indiv = self.population.best_individual
 
         # Opens a file and writes the data of the running on it
-        with open(f"{factory.name}.csv", "w") as f:
+        with open(f"data/{factory.name}.csv", "w") as f:
             f.write("generation,best,avg,worst\n")
 
+            # Start a loop for performing all the generations
             for i in range(self.n_gen):
                 new_individuals = []
 
+                # Each loop creates a new Individual 
                 while len(new_individuals) < self.pop_size:
+                    # Selects from a "tournament" the best two in them
                     ind1 = self.population.tournament(self.tour_size)
                     ind2 = self.population.tournament(self.tour_size)
 
+                    # If it complies a crossover is performed
                     if random.random() < self.px:
                         child = op.ox(ind1, ind2)
                     else:
                         child = ind1.copy()
                     
+                    # If it complies a mutation is performed
                     if random.random() < self.pm:
                         child = op.inversion(child)
                     
+                    # Sees if the child is the best at the moment
                     child.evaluate(factory)
                     if child.fitness <= self.best_indiv.fitness:
                         self.best_indiv = child
                     
+                    # Adds it to the new population
                     new_individuals.append(child)
 
+                # Saves the new population
                 self.population.individuals = new_individuals
+                #Logs the data of the generation
                 self._log(i, f)
 
         return self.best_indiv
